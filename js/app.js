ddccd1d2387660148a813369f2f2aed1b1f88403
42yeah <potion@live.cn>
2019-01-15 00:34:52 +0800

    new stuff!

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const translatorWidget = document.getElementsById("MicrosoftTranslatorWidget")
+
+translatorWidget.onchange = function() {
+  translatorWidget.style.backgroundColor = rgb(85, 85, 85)
+}

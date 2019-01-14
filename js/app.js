cb41c93c4c1fa78066355c1b6dc2ae9471039867
42yeah <potion@live.cn>
2019-01-15 00:35:48 +0800

    stupid typo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const translatorWidget = document.getElementsById("MicrosoftTranslatorWidget")
+const translatorWidget = document.getElementById("MicrosoftTranslatorWidget")
 
 translatorWidget.onchange = function() {
   translatorWidget.style.backgroundColor = rgb(85, 85, 85)

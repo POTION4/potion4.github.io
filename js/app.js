8e4e642ee243951b060dd7807b3317145ff003c3
42yeah <potion@live.cn>
2019-01-15 00:41:32 +0800

    hmmm,m

--- js/app.js
+++ js/app.js
@@ -4,9 +4,11 @@ const onload = window.onload
 window.onload = function() {
   if (onload) { onload() }
 
+  console.log("onload called")
+
   const translatorWidget = document.getElementById("MicrosoftTranslatorWidget")
 
   translatorWidget.onchange = function() {
-    translatorWidget.style.backgroundColor = rgb(85, 85, 85)
+    translatorWidget.style.backgroundColor = "rgb(85, 85, 85)"
   }
 }
